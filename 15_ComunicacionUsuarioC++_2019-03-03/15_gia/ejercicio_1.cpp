bf943c323789bb32cd43ff64bee75b9e964047f7
julian343434 <julher2000@hotmail.es>
2019-03-15 19:45:51 -0500

    haciendo los ejercicios de la gia 15

---
+++ 15_ComunicacionUsuarioC++_2019-03-03/15_gia/ejercicio_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    cout<<"4 3 2 1 \n";
+    cout<<" 4 "<<" 3 "<<" 2 "<<" 1 \n";
+    cout<<"4\n";
+    cout<<"3\n";
+    cout<<"2\n";
+    cout<<"1\n";
+return 0;
+}
