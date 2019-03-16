3e4bf3cb926d7bcbe5f137635d3e77123996efd5
julian343434 <julher2000@hotmail.es>
2019-03-16 12:25:56 -0500

    haciendo el ejercicio 3 y 2

---
+++ 20_Algoritmia-2019-03-10/capitulo3/capitulo3/ejercicio_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class LibroCalificaciones
+{
+public;
+    void mostrarMensaje( string nombreCurso) const
+    {
+        cout<<"bienvenidos al libro de calificaciones\n"<<nombreCurso<<"!"
+        <<endl;
+    }
+};
+int main()
+{
+    string nombreDelCurso;
+    LibroCalificaciones miLibroCalificaciones;
+    cout<<"escribir el nombre del curso !"<<endl;
+    getline( cin,nombreCurso);
+    cout<<endl;
+    mi LibroCalificaciones.mostrarMensaje( nombreDelCurso)
+}
